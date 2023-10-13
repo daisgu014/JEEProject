4da0db9651d513a345535371348998e29393d6e8
daisgu014 <daibucky@gmail.com>
2023-10-13 20:46:04 +0700

    delete code provider

--- src/main/java/com/JEEProject/TableStore/controller/ProviderController.java
+++
@@ -1,9 +0,0 @@
-package com.JEEProject.TableStore.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(path = "admin/providers")
-public class ProviderController {
-}
--- src/main/java/com/JEEProject/TableStore/repositories/ProviderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.JEEProject.TableStore.repositories;
-
-import com.JEEProject.TableStore.Model.Provider;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProviderRepository extends CrudRepository<Provider, Integer> {
-}
