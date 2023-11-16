1640e8bad35aa6eeb1477dfb24b026251e131ebf
hau <lnhau94@gmail.com>
2023-11-16 17:22:11 +0700

    chua nhin thay chart

--- src/main/java/com/JEEProject/TableStore/controller/StatisticController.java
+++ src/main/java/com/JEEProject/TableStore/controller/StatisticController.java
@@ -5,22 +5,40 @@ import com.JEEProject.TableStore.Model.Order;
 import com.JEEProject.TableStore.services.StatisticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+
+@Controller
+@RequestMapping(path = {"/admin/statistic"})
+@PreAuthorize("hasRole('ADMIN')")
 public class StatisticController {
 
     @Autowired
     private StatisticService st;
 
 
-    @GetMapping("/statistic")
+    @GetMapping(value = "")
     public ResponseEntity<Map<Account, List<Order>>> getOrder(){
         return ResponseEntity.ok(st.groupOrderByAccount());
+    }
+
+    @RequestMapping(path = "/customer")
+    @ResponseBody
+    public List<List<Object[]>> findTop5Customer(){
+        try {
+
+            return List.of(st.top5Customer(11,2023),st.top5Saler(11,2023));
+        } catch (ParseException e) {
+            return new ArrayList<>();
+        }
     }
 }
--- src/main/java/com/JEEProject/TableStore/repositories/OrderRepo.java
+++ src/main/java/com/JEEProject/TableStore/repositories/OrderRepo.java
@@ -2,10 +2,27 @@ package com.JEEProject.TableStore.repositories;
 
 
 import com.JEEProject.TableStore.Model.Order;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
 
 @Repository
 public interface OrderRepo extends CrudRepository<Order,Integer> {
+
+    @Query( "select user_id, COUNT(*) orderCount, SUM(total_price) total_price " +
+            "from Order o " +
+            "where o.create_at BETWEEN :sd AND :ed " +
+            "GROUP BY user_id ")
+    public List<Object[]> findTop5Customer(@Param("sd") Date sd, @Param("ed") Date ed);
+
+    @Query( "select confirm_id, COUNT(*) orderCount, SUM(total_price) total_price " +
+            "from Order o " +
+            "where o.create_at BETWEEN :sd AND :ed " +
+            "GROUP BY confirm_id ")
+    public List<Object[]> findTop5Saler(@Param("sd") Date sd, @Param("ed") Date ed);
 
 }
